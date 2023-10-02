dd0840c11686ce373804a754580262936fde2a05
SeptinaOktafia <oktafiaseptina64@gmail.com>
2023-10-02 14:00:03 +0700

    tes

--- app/src/main/java/id/rhptl/mobileprograming/MainActivity.kt
+++ app/src/main/java/id/rhptl/mobileprograming/MainActivity.kt
@@ -16,28 +16,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btn: Button
     private lateinit var result: TextView
 
+    companion object{
+        private const val STATE_RESULT ="state_result"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // inisialisasi variabel
-        inputpanjang= findViewById(R.id.edt_length)
-        inputtinggi= findViewById(R.id.edt_width)
-        inputLebar= findViewById(R.id.edt_height)
+        inputpanjang = findViewById(R.id.edt_length)
+        inputLebar = findViewById(R.id.edt_height)
+        inputtinggi = findViewById(R.id.edt_width)
+        btn = findViewById(R.id.btn_result)
+        result = findViewById(R.id.result)
 
         btn.setOnClickListener(this)
+
+        //menampilkan value yang ada di budle
+        if (savedInstanceState !=null) {
+            result.text=savedInstanceState.getString(STATE_RESULT)
+        }
 
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT,result.text.toString());
+    }
+
+
+
     override fun onClick(v: View?) {
         if (v?.id==R.id.btn_result){
+            var panjang = inputpanjang.text.toString().trim()
+            var lebar = inputLebar.text.toString().trim()
             var tinggi = inputtinggi.text.toString().trim()
-            var panjang = inputLebar.text.toString().trim()
-            var Lebar = inputLebar.text.toString().trim()
-            var volume = tinggi.toDouble() * panjang.toDouble() * Lebar.toDouble()
+            var volume = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
             result.text = volume.toString()
         }
 
     }
+
+
 }
